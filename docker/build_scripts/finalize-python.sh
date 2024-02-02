5f6ae641056cc4363a060f968a2075c199cf69e9
Leo Singer <leo.singer@ligo.org>
2024-02-02 10:22:25 -0500

    Enable libpython shared libraries

--- docker/build_scripts/finalize-python.sh
+++ docker/build_scripts/finalize-python.sh
@@ -3,11 +3,11 @@
 # Stop at any error, show all commands
 set -exuo pipefail
 
-# most people don't need libpython*.a, and they're many megabytes.
+# most people don't need libpython*.*, and they're many megabytes.
 # compress them all together for best efficiency
 pushd /opt/_internal
-XZ_OPT=-9e tar -cJf static-libs-for-embedding-only.tar.xz cpython-*/lib/libpython*.a
+XZ_OPT=-9e tar -cJf static-libs-for-embedding-only.tar.xz cpython-*/lib/libpython*.*
 popd
-find /opt/_internal -name '*.a' -print0 | xargs -0 rm -f
+find /opt/_internal -name 'libpython*.*' -print0 | xargs -0 rm -f
 
 hardlink -cv /opt/_internal
